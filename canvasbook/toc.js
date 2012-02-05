41a96785e64befe01db81942078cc9a998d96e06
Josh Marinacci <joshua@marinacci.org>
2012-02-05 15:12:59 -0800

    make toc more automatic

--- canvasbook/toc.js
+++ canvasbook/toc.js
@@ -6,3 +6,8 @@ $("#fchapter").click(function(){
 		toc.style.visibility = "visible";
 	}
 });
+
+$("#toc").append('<li><a href="toc.html">Table Of Contents</a></li>');
+$("#toc").append('<li><a href="chapter1.html">Chapter 1 : Overview</a></li>');
+$("#toc").append('<li><a href="chapter2.html">Chapter 2 : Building Charts</a></li>');
+
