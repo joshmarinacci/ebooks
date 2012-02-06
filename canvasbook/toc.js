282a55a7c5a4c1207b66cafb47afd65f0d498b6f
Josh Marinacci <joshua@marinacci.org>
2012-02-05 16:59:57 -0800

    added font sizing

--- canvasbook/toc.js
+++ canvasbook/toc.js
@@ -8,6 +8,19 @@ $("#fchapter").click(function(){
 });
 
 $("#toc").append('<li><a href="toc.html">Table Of Contents</a></li>');
-$("#toc").append('<li><a href="chapter1.html">Chapter 1 : Overview</a></li>');
-$("#toc").append('<li><a href="chapter2.html">Chapter 2 : Building Charts</a></li>');
+$("#toc").append('<li><a href="chapter1.html"><b>Chapter 1</b> : Overview</a></li>');
+$("#toc").append('<li><a href="chapter2.html"><b>Chapter 2</b> : Hands On: Graphs and Charts</a></li>');
+$("#toc").append('<li><a href="chapter3.html"><b>Chapter 3</b> : Advanced Drawing</a></li>');
+
+
+/* font resize commands */
+$("#set-font-small").click(function(e) {
+    $("body").css('font-size','10pt');
+});
+$("#set-font-med").click(function(e) {
+    $("body").css('font-size','13pt');
+});
+$("#set-font-large").click(function(e) {
+    $("body").css('font-size','18pt');
+});
 
