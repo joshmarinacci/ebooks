55234f0df59a10532fcadefaf0e6dd95af2a068f
Josh Marinacci <joshua@marinacci.org>
2012-02-16 12:36:56 -0800

    added preformatting

--- canvasbook/setup.js
+++ canvasbook/setup.js
@@ -88,6 +88,25 @@ $(document).ready(function () {
 });
 
 
+/* restyle the pre code */
+$(document).ready(function(){
+	$("pre code").each(function(){
+		var text = $(this).text();
+		var lines = text.split("\n"); 
+		var newText = "";
+		for(var i=0; i<lines.length; i++) {
+			var line = lines[i];
+			line = line.replace(/</g,'&lt;');
+			line = line.replace(/>/g,'&gt;');
+			newText += "<span>"+line+" </span>";
+		}
+		
+		$(this).html(newText);
+		
+	});
+});
+
+
     
     
 
